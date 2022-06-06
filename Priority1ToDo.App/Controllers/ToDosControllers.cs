78441912037a366193f9c303d0497c5fcc943fb0
Kyle Morton <kylemortondevelopment@gmail.com>
2022-06-05 19:36:13 -0500

    wip

---
+++ Priority1ToDo.App/Controllers/ToDosControllers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Priority1ToDo.App.Controllers
+{
+    public class ToDosControllers : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
